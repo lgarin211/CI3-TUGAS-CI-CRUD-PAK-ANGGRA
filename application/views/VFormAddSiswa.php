d3b53e1a3c082cd8862bd7dff251109ffc6565e9
lgarin211 <lagustinus211@gmail.com>
2021-03-29 14:04:55 +0700

    selesai3

--- application/views/VFormAddSiswa.php
+++ application/views/VFormAddSiswa.php
@@ -2,5 +2,5 @@
     NIS<input class="form-control" type="text" name="Nis"><br>
     Nama<input class="form-control" type="text" name="Nama"><br>
     No_telp<input class="form-control" type="text" name="No_telp"><br>
-    <input class="form-control btn btn-prymary" type="submit" name="simpan" value="Simpan">
+    <input class="form-control btn btn-primary" type="submit" name="simpan" value="Simpan">
 </form>
