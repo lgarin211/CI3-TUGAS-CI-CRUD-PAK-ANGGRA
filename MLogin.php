05c97bc5e8fbcb965c0c3ef40c96f2594e0212e7
lgarin211 <lagustinus211@gmail.com>
2021-03-29 12:46:56 +0700

    tugas CI

---
+++ MLogin.php
@@ -0,0 +1,22 @@
+<?php
+	class MLogin extends CI_Model{
+		function __construct(){
+		parent::__construct();
+		}
+
+		function GoLogin($username,$password){
+			$this->db->select('*');
+			$this->db->from('tbl_login');
+			$this->db->where('username', $username);
+			$this->db->where('password', $password);
+			$query = $this->db->get();
+			if($query -> num_rows() == 1){
+				$row = $query->row(); 
+    			return $row->nip;
+			}else{
+				return false;
+			}
+		}
+
+	}
+?>
---
+++ MSudi.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MSudi extends CI_Model
+{
+    function AddData($tabel, $data=array())
+    {
+        $this->db->insert($tabel,$data);
+    }
+
+    function UpdateData($tabel,$fieldid,$fieldvalue,$data=array())
+    {
+        $this->db->where($fieldid,$fieldvalue)->update($tabel,$data);
+    }
+
+    function DeleteData($tabel,$fieldid,$fieldvalue)
+    {
+        $this->db->where($fieldid,$fieldvalue)->delete($tabel);
+    }
+
+    function GetData($tabel)
+    {
+        $query= $this->db->get($tabel);
+        return $query->result();
+    }
+
+    function GetDataWhere($tabel,$id,$nilai)
+    {
+        $this->db->where($id,$nilai);
+        $query= $this->db->get($tabel);
+        return $query;
+    }
+
+}
